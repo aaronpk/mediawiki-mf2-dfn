2a5c04682747ea5a4f853a2c32640c751ea4667e
Aaron Parecki <aaron@parecki.com>
2014-05-13 10:24:49 -0700

    create dfn.php

---
+++ dfn.php
@@ -0,0 +1,18 @@
+<?php
+
+$wgHooks['ParserBeforeStrip'][] = 'MF2dfn::onParserBeforeStrip';
+
+class MF2dfn {
+        public static function onParserBeforeStrip(&$parser, &$text, &$strip_state) {
+                static $hasRun = false;
+                if($hasRun) return true;
+
+                if(!preg_match('/p-summary/', $text)) {
+                        if(preg_match('/.*<dfn>.+<\/dfn>.+?[.:](?:\s|$)/', $text, $match)) {
+                                $text = str_replace($match[0], '<span class="p-summary">'.$match[0].'</span>', $text);
+                                $hasRun =  true;
+                        }
+                }
+                return true;
+        }
+}
